64edd09c18cd09bbd062cb1d50a9432505b3ab02
boodah <boodah21@protonmail.com>
2018-09-25 11:21:34 -0500

    Se leen archivos sincrona y asincronamente. Video#3

--- NodeJS/appV3.js
+++ NodeJS/appV3.js
@@ -7,10 +7,11 @@ var fs = require('fs');
 //var html = fs.readFileSync("./HTML/indexV3.html"); // modificar a html el html para hacerlo correr sincronamente (obvio saco la coneccion del servidor fuera)
 
 //leer un archivo de forma asincrona
-fs.readFileSync("./HTML/indexV3.html",(err,html)=>{
-    http.createServer((req, res)=>{
+
+
+http.createServer((req, res)=>{
+    fs.readFile("./HTML/indexV3.html",(err,html)=>{
         res.write(html);
         res.end();
-    }).listen(5000); // crea el servidor http
-});
-
+    });
+}).listen(5000); // crea el servidor http
